a82c351f27573ea9b6bf8ffd90cba9356404ad10
Mohammadaman <majmeri@flemingcollege.ca>
2023-07-25 21:54:15 -0400

    final script
    fully completed script

--- create_relationships.py
+++ create_relationships.py
@@ -7,10 +7,15 @@ Usage:
  python create_relationships.py
 """
 import os
+import sqlite3
+from random import randint, choice
+from faker import Faker
 
 # Determine the path of the database
 script_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(script_dir, 'social_network.db')
+con = sqlite3.connect('social_network.db')
+cur = con.cursor()
 
 def main():
     create_relationships_table()
@@ -20,12 +25,48 @@ def create_relationships_table():
     """Creates the relationships table in the DB"""
     # TODO: Function body
     # Hint: See example code in lab instructions entitled "Add the Relationships Table"
+    create_relationships_table = """
+        CREATE TABLE IF NOT EXISTS relationships
+        (
+        id INTEGER PRIMARY KEY,
+        person1_id INTEGER NOT NULL,
+        person2_id INTEGER NOT NULL,
+        type TEXT NOT NULL,
+        start_date DATE NOT NULL,
+        FOREIGN KEY (person1_id) REFERENCES people (id),
+        FOREIGN KEY (person2_id) REFERENCES people (id)
+    );
+"""
+    cur.execute(create_relationships_table)
+    con.commit()
+    con.close()
     return
 
 def populate_relationships_table():
     """Adds 100 random relationships to the DB"""
     # TODO: Function body
     # Hint: See example code in lab instructions entitled "Populate the Relationships Table"
+    populate_relationships_table = """
+        INSERT INTO relationships
+        (
+            person1_id,
+            person2_id,
+            type,
+            start_date
+        )
+        VALUES (?, ?, ?, ?);
+""" 
+    fake = Faker()
+    person1_id = randint(1, 100)
+    person2_id = randint(1, 100)
+    while person2_id == person1_id:
+        person2_id = randint(1, 100)
+    relation_type = choice(('spouse'))
+    start_date = fake.date_between(start_date='-50y', end_date='today')
+    new_relation = (person1_id, person2_id, relation_type, start_date)
+    cur.execute(populate_relationships_table, new_relation)
+    con.commit()
+    con.close()
     return 
 
 if __name__ == '__main__':
--- marriage_report.py
+++ marriage_report.py
@@ -9,6 +9,7 @@ Usage:
 import os
 import sqlite3
 from create_relationships import db_path, script_dir
+import pandas as pd
 
 def main():
     # Query DB for list of married couples
@@ -27,6 +28,17 @@ def get_married_couples():
     # TODO: Function body
     # Hint: See example code in lab instructions entitled "Get a List of Relationships"
     con = sqlite3.connect(db_path)
+    cur = con.cursor
+    all_relationships_query = """
+        SELECT person1.name, person2.name, start_date, type FROM relationships
+        JOIN people person1 ON person1_id = person1.id
+        JOIN people person2 ON person2_id = person2.id;
+"""
+    cur.execute(all_relationships_query)
+    all_relations = cur.fetchall()
+    con.close()
+    for person1, person2, start_date, type in all_relations:
+        print(f'{person1} has been a {type} of {person2} since {start_date}.')
     return
 
 def save_married_couples_csv(married_couples, csv_path):
@@ -38,8 +50,11 @@ def save_married_couples_csv(married_couples, csv_path):
         csv_path (str): Path of CSV file
     """
     # TODO: Function body
-    # Hint: We did this in Lab 7.
+    df = pd.read_csv(csv_path)
+    df.insert(columns=['Name', 'Age'], inplace=True)
+    df = pd.save(csv_path)
     return
+    # Hint: We did this in Lab 7.
 
 if __name__ == '__main__':
    main()
